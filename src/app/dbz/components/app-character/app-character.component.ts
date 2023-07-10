e6f3bca3b928182241f3c71a7bab14b794f2251f
joel encarnacion <joel12mateo@gmial.com>
2023-07-09 22:36:46 -0300

    dbz componentes

---
+++ src/app/dbz/components/app-character/app-character.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-character',
+  templateUrl: './app-character.component.html',
+  styleUrls: ['./app-character.component.css']
+})
+export class AppCharacterComponent {
+
+}
